1b3af7282ad8c920ed488c0df9a23000715f2130
Octoping925 <mooner925@gmail.com>
2022-08-01 19:16:43 +0900

    클라이언트, 서버 파일 추가

---
+++ talboardserver/src/main/java/com/talmo/talboard/TalboardApplication.java
@@ -0,0 +1,13 @@
+package com.talmo.talboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalboardApplication.class, args);
+	}
+
+}
---
+++ talboardserver/src/test/java/com/talmo/talboard/TalboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talmo.talboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
