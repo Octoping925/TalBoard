c93ffaf03e64c57ebf81e0cacad729926a2ed17f
Octoping <myc1365@naver.com>
2022-09-21 00:11:15 +0900

    STYLE: 불필요한 탭 제거

--- talboardserver/src/main/java/com/talmo/talboard/controller/FileController.java
+++ talboardserver/src/main/java/com/talmo/talboard/controller/FileController.java
@@ -20,52 +20,52 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-    @Slf4j
-    @Controller
-    public class FileController {
+@Slf4j
+@Controller
+public class FileController {
 
-        private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
-        @Autowired
-        private FileService fileService;
+    @Autowired
+    private FileService fileService;
 
-        @PostMapping(value = "/files/upload")
-        public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
-            String fileName = fileService.saveFile(file);
-            if(file.isEmpty()) {
-                /* 파일을 업로드 하지 않았을 경우 처리 */
-            }
-
-            String downloadURI = ServletUriComponentsBuilder.fromCurrentContextPath()
-                    .path("/files/download/")
-                    .path(fileName)
-                    .toUriString();
-
-            return new ResponseEntity<>(downloadURI, HttpStatus.OK);
+    @PostMapping(value = "/files/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileService.saveFile(file);
+        if(file.isEmpty()) {
+            /* 파일을 업로드 하지 않았을 경우 처리 */
         }
 
-        @GetMapping(value = "/files/download/{fileName:.+}")
-        public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws FileNotFoundException {
+        String downloadURI = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/download/")
+                .path(fileName)
+                .toUriString();
 
-            Resource resource = fileService.loadFile(fileName);
-
-            String contentType = null;
-            try {
-                contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-            } catch (IOException ex) {
-                log.info("Could not determine file type.");
-            }
-
-            // Fallback to the default content type if type could not be determined
-            if(contentType == null) {
-                contentType = "application/octet-stream";
-            }
-            return ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType(contentType))
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                    .body(resource);
-        }
-
+        return new ResponseEntity<>(downloadURI, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/files/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws FileNotFoundException {
+
+        Resource resource = fileService.loadFile(fileName);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            log.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+}
 
 
